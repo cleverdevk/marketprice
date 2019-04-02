2a3a43509c44cbf53e1b9279d3934eddbbec1625
inbae Kang <cleverdevk@gmail.com>
2019-04-03 00:46:57 +0900

    add sources

--- androidProject/app/src/main/java/com/example/marketprice/InputTransportActivity.java
+++ androidProject/app/src/main/java/com/example/marketprice/InputTransportActivity.java
@@ -1,4 +1,45 @@
 package com.example.marketprice;
 
-public class InputTransportActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.util.Log;
+
+public class InputTransportActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inputtransport);
+
+        final Spinner spinner_field = (Spinner) findViewById(R.id.spinner_field);
+        String[] str = getResources().getStringArray(R.array.spinnerArray);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.spinner_item,str);
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_field.setAdapter(adapter);
+
+        spinner_field.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner_field.getSelectedItemPosition() > 0){
+                    // 선택된 것
+                    Log.v("알림",spinner_field.getSelectedItem().toString()+"is selected");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
 }
