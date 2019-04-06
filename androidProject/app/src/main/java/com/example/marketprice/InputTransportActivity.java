ea40624cdd42ff5659b4332b69766c59f79fa54c
inbae Kang <cleverdevk@gmail.com>
2019-04-06 13:37:28 +0900

    input Activity

--- androidProject/app/src/main/java/com/example/marketprice/InputTransportActivity.java
+++ androidProject/app/src/main/java/com/example/marketprice/InputTransportActivity.java
@@ -1,10 +1,12 @@
 package com.example.marketprice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.util.Log;
 
@@ -16,12 +18,33 @@ public class InputTransportActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState)
     {
 
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inputtransport);
 
         final Spinner spinner_field = (Spinner) findViewById(R.id.spinner_field);
         String[] str = getResources().getStringArray(R.array.spinnerArray);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.spinner_item,str);
+
+        Button mBtnDeparture, mBtnDestination;
+        mBtnDeparture = (Button) findViewById(R.id.BtnSelectDeparture);
+        mBtnDestination=(Button) findViewById(R.id.BtnSelectDestination);
+
+        mBtnDeparture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InputTransportActivity.this, SelectPositionActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBtnDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InputTransportActivity.this, SelectPositionActivity.class);
+                startActivity(intent);
+            }
+        });
 
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         spinner_field.setAdapter(adapter);
@@ -39,6 +62,8 @@ public class InputTransportActivity extends Activity {
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
+
+
         });
 
     }
---
+++ androidProject/app/src/main/java/com/example/marketprice/SelectPositionActivity.java
@@ -0,0 +1,14 @@
+package com.example.marketprice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectPositionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectposition);
+    }
+}
