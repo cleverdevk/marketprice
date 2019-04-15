ff6c5a513aae6ceb35b950369e5e1e906bfcafd7
inbae Kang <cleverdevk@gmail.com>
2019-04-15 21:48:10 +0900

    complete Directions API & Database Communication is not implemented.

--- androidProject/app/src/main/java/com/example/marketprice/InputTransportActivity.java
+++ androidProject/app/src/main/java/com/example/marketprice/InputTransportActivity.java
@@ -1,21 +1,28 @@
 package com.example.marketprice;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.util.Log;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -44,6 +51,10 @@ public class InputTransportActivity extends FragmentActivity implements MapFragm
     Position mDeparture = new Position();
     Position mDestination = new Position();
     GoogleMap googleMap;
+    String mDepartureAddress, mDestinationAddress, mDistance, mType, mCost, mTime;
+    EditText mEtAmount;
+    Switch switchAccouting;
+    boolean isShare;
 
 
 
@@ -66,15 +77,68 @@ public class InputTransportActivity extends FragmentActivity implements MapFragm
         final Spinner spinner_field = (Spinner) findViewById(R.id.spinner_field);
         String[] str = getResources().getStringArray(R.array.spinnerArray);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.spinner_item,str);
+        switchAccouting = (Switch) findViewById(R.id.switchAccounting);
+
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage("정보를 모두 입력해 주세요.");
+        alertBuilder.setPositiveButton("확인",null);
+
+        final AlertDialog alert = alertBuilder.create();
+        alert.setIcon(R.drawable.ic_launcher_foreground);
+        alert.setTitle("교통 정보 입력");
 
 
         pager = (ViewPager)findViewById(R.id.pager);
 
         pager.setAdapter(new pagerAdapter(getSupportFragmentManager()));
 
-        final Button mBtnDeparture, mBtnDestination;
+        final Button mBtnDeparture, mBtnDestination, mBtnOK;
         mBtnDeparture = (Button) findViewById(R.id.btnDeparture);
         mBtnDestination=(Button) findViewById(R.id.btnDestination);
+        mBtnOK = (Button) findViewById(R.id.btnOK);
+        mEtAmount = (EditText) findViewById(R.id.editTextAmount);
+
+        switchAccouting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setData(spinner_field.toString(),switchAccouting.isChecked());
+            }
+        });
+
+
+
+        mEtAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setData(spinner_field.toString(),true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setData(spinner_field.toString(),true);
+            }
+        });
+
+
+        mBtnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isDataReady()){
+                    //push data to db
+                }
+                else{
+
+
+                    alertBuilder.create().show();
+                }
+
+            }
+        });
 
         mBtnDeparture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,16 +163,19 @@ public class InputTransportActivity extends FragmentActivity implements MapFragm
                     mDestination.y = current.longitude;
 
                     if(mDestination.x != 0 && mDestination.y != 0){
-                        String basereq = "https://maps.googleapis.com/maps/api/distancematrix/json?unit=metric&";
-                        String API_KEY = "AIzaSyC9fsk433XYbQqE8X1mMEkFtij6G2tGRlk";
+                        String basereq = "https://maps.googleapis.com/maps/api/directions/json?unit=metric&";
+                        //String API_KEY = "AIzaSyC9fsk433XYbQqE8X1mMEkFtij6G2tGRlk";
+                        String API_KEY = "AIzaSyClJpA5YRWaLkc7hXplUolDaCxFXtasK1k";
                         String origins = Double.toString(mDeparture.x) + "," + Double.toString(mDeparture.y);
                         String destinations = Double.toString(mDestination.x) + "," + Double.toString(mDestination.y);
-                        String REQ = basereq + "origins=" + origins + "&destinations=" + destinations + "&key=" + API_KEY;
+                        String REQ = basereq + "origin=" + origins + "&destination=" + destinations + "&key=" + API_KEY;
+                        Log.d("[INBAE]","REQUSET SQL = " + REQ);
                         StringRequest stringRequest = new StringRequest(REQ, new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
                                 Log.d("[INBAE]", response);
                                 drawPath(response);
+                                setData(response,spinner_field.getSelectedItem().toString(),isShare);
                             }
                         }, new Response.ErrorListener() {
                             @Override
@@ -134,6 +201,7 @@ public class InputTransportActivity extends FragmentActivity implements MapFragm
                 if(spinner_field.getSelectedItemPosition() > 0){
                     // 선택된 것
                     Log.v("알림",spinner_field.getSelectedItem().toString()+"is selected");
+                    setData(spinner_field.toString());
                 }
             }
 
@@ -171,6 +239,46 @@ public class InputTransportActivity extends FragmentActivity implements MapFragm
         }
     }
 
+    public boolean isDataReady(){
+        return mType != null && mDestinationAddress != null && mDepartureAddress != null && mTime != null && mDistance != null && mCost != null;
+    }
+
+    public  void setData(String result, String spinner_text, boolean switchValue){
+        final JSONObject json;
+        try {
+            json = new JSONObject(result);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routePlace0 = routeArray.getJSONObject(0);
+            JSONArray routePlace1 = routePlace0.getJSONArray("legs");
+            JSONObject routePlace2 = routePlace1.getJSONObject(0);
+            JSONObject routeDistance = routePlace2.getJSONObject("distance");
+            JSONObject routeTime = routePlace2.getJSONObject("duration");
+
+            mDistance = routeDistance.getString("text");
+            mTime = routeTime.getString("text");
+            mDepartureAddress = routePlace2.getString("start_address");
+            mDestinationAddress = routePlace2.getString("end_address");
+            mType = spinner_text;
+            isShare = switchValue;
+            mCost = mEtAmount.getText().toString();
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void setData(String spinner_text, boolean switchValue){
+        mType = spinner_text;
+        isShare = switchValue;
+        mCost = mEtAmount.getText().toString();
+    }
+
+    public void setData(String spinner_text){
+        mType = spinner_text;
+        mCost = mEtAmount.getText().toString();
+    }
+
     public void drawPath(String  result) {
         //Getting both the coordinates
         LatLng from = new LatLng(mDeparture.x,mDeparture.y);
@@ -191,7 +299,7 @@ public class InputTransportActivity extends FragmentActivity implements MapFragm
             JSONObject routes = routeArray.getJSONObject(0);
             JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
             String encodedString = overviewPolylines.getString("points");
-            Toast.makeText(this,String.valueOf(encodedString),Toast.LENGTH_LONG);
+            Log.d("[INBAE]",encodedString);
             List<LatLng> list = decodePoly(encodedString);
             Polyline line = googleMap.addPolyline(new PolylineOptions()
                     .addAll(list)
