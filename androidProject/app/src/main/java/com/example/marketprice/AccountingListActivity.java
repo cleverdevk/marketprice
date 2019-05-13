27f02fb8f61c8c99e8f8564b4aa2a1bc0dbc3c16
inbae Kang <cleverdevk@gmail.com>
2019-05-13 23:50:59 +0900

    connect with database to show user's accounting

--- androidProject/app/src/main/java/com/example/marketprice/AccountingListActivity.java
+++ androidProject/app/src/main/java/com/example/marketprice/AccountingListActivity.java
@@ -3,47 +3,102 @@ package com.example.marketprice;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.marketprice.databinding.ActivityAccountinglistBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
 public class AccountingListActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
-    private String[] names = {"LALALAND 따라 가는 LA 여행","Google! Apple! 컴공의 성지 탐방 Sanfrancisco 여행!","타코, 나초, 모히또 맛있는 것이 넘쳐나는 San Diego 여행!","종강기념 삿포로 조지기","기타등등","기타등등2","기타등등3","기타등등4","기타등등5"};
+    //private String[] names = {"LALALAND 따라 가는 LA 여행","Google! Apple! 컴공의 성지 탐방 Sanfrancisco 여행!","타코, 나초, 모히또 맛있는 것이 넘쳐나는 San Diego 여행!","종강기념 삿포로 조지기","기타등등","기타등등2","기타등등3","기타등등4","기타등등5"};
+    private ArrayList<String> names;
     private static final int LAYOUT = R.layout.activity_accountinglist;
     private ActivityAccountinglistBinding mainBinding;
     private RecyclerView.Adapter adapter;
     ImageButton imageButton;
+    Toolbar toolbar;
+    final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            adapter.notifyDataSetChanged();
+        }
+    };
 
     private ArrayList<AccountingListItem> mItems = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = DataBindingUtil.setContentView(this,LAYOUT);
+
         setRecyclerView();
         setRefresh();
-        imageButton = (ImageButton) findViewById(R.id.toolbar2_addButton);
+        getSupportActionBar().setTitle("내 가계부");
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),AccountingWriteActivity.class);
-                startActivity(intent);
 
-            }
-        });
+
+        //imageButton = (ImageButton) findViewById(R.id.toolbar2_addButton);
+
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(),AccountingWriteActivity.class);
+//                startActivity(intent);
+//
+//            }
+//        });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.myaccounting, menu);
+        return true;
+    }
+    //액션버튼을 클릭했을때의 동작
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        //or switch문을 이용하면 될듯 하다.
+        if (id == R.id.action_add) {
+            Toast.makeText(this, "추가", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(),AccountingWriteActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     private void setRecyclerView(){
         // 각 Item 들이 RecyclerView 의 전체 크기를 변경하지 않는 다면
@@ -81,7 +136,7 @@ public class AccountingListActivity extends AppCompatActivity implements SwipeRe
                         Toast.makeText(getApplicationContext(),position+"번 째 아이템 롱 클릭",Toast.LENGTH_SHORT).show();
                     }
                 }));
-        setData();
+        PostData();
     }
 
     private void setRefresh(){
@@ -101,13 +156,61 @@ public class AccountingListActivity extends AppCompatActivity implements SwipeRe
         },500);
     }
 
-    private void setData(){
-        mItems.clear();
-        // RecyclerView 에 들어갈 데이터를 추가합니다.
-        for(String name : names){
-            mItems.add(new AccountingListItem(name));
-        }
-        // 데이터 추가가 완료되었으면 notifyDataSetChanged() 메서드를 호출해 데이터 변경 체크를 실행합니다.
-        adapter.notifyDataSetChanged();
+//    private void setData(){
+//        mItems.clear();
+//        PostData();
+//        // RecyclerView 에 들어갈 데이터를 추가합니다.
+//        if(names != null) {
+//            for (String name : names) {
+//                mItems.add(new AccountingListItem(name));
+//            }
+//        }
+//        // 데이터 추가가 완료되었으면 notifyDataSetChanged() 메서드를 호출해 데이터 변경 체크를 실행합니다.
+//        adapter.notifyDataSetChanged();
+//    }
+
+
+   public void PostData(){
+       mItems.clear();
+        OkHttpClient client = new OkHttpClient();
+        String result;
+        RequestBody body= new FormBody.Builder()
+                .add("id","123").build();
+
+        Request request = new Request.Builder()
+                .url("http://ec2-13-125-178-212.ap-northeast-2.compute.amazonaws.com/php/getAccounting.php")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.d("[INBAE_FAILURE]",mMessage);
+            }
+
+            @Override
+            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                String mMessage = response.body().string();
+                names = new ArrayList<>();
+                try {
+                    JSONArray json = new JSONArray(mMessage);
+
+                    for(int i=0;i<json.length();i++){
+                        JSONObject jsonObject = json.getJSONObject(i);
+                        names.add(jsonObject.getString("title"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d("[INBAE]",names.toString());
+                Log.d("[INBAE_SUCCESS]",mMessage);
+                for (String name : names) {
+                    mItems.add(new AccountingListItem(name));
+                }
+                handler.sendMessage(new Message());
+            }
+        });
+       // 데이터 추가가 완료되었으면 notifyDataSetChanged() 메서드를 호출해 데이터 변경 체크를 실행합니다.
+
     }
 }
